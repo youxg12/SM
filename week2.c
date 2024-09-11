bfaa99229bb261cfa5811be5342ef1bbfc3eb811
youxg12 <zero_121@sookmyung.ac.kr>
2024-09-11 14:23:13 +0900

    week2

--- week2.c
+++ week2.c
@@ -4,7 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello, Sookmyung Electroncis!\n");
-	return 0;
+	int a, b;
 	
+	a=10;
+	b=20;
+	
+	printf("Hello, Sookmyung Electroncis!\n");
+	printf("%d + %d = %d\n", a, b, a+b);
+	return 0;
 }
