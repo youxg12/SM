4e2158e89318999f997534da84a9b4323d14c697
youxg12 <zero_121@sookmyung.ac.kr>
2024-09-25 13:59:37 +0900

    2

---
+++ week3/2/2.c
@@ -0,0 +1,19 @@
+#include <windows.h>
+
+#include <stdio.h>
+
+int main(void)
+{
+    char c; 
+    int i;
+
+    printf("input a number : ");
+    scanf("%c", &c);  
+
+    i = c - '0';  // 문자를 숫자로 변환
+
+    printf("The input number is %i\n", i);
+
+    return 0;
+}
+
