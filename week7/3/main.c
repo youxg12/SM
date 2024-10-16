78393214b99df82c235684102ca0925a5f011ae2
youxg12 <zero_121@sookmyung.ac.kr>
2024-10-16 14:19:23 +0900

    project 3

---
+++ week7/3/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*
+void square(int a) {
+    a = a * a;
+}
+
+int main() {
+    int a = 2;
+    square(a);
+    printf("result1 : a=%i\n", a); 
+    return 0;
+}
+*/
+
+/*
+int square(int a) {
+    return (a * a);
+}
+
+int main() {
+    int a = 2;
+    a = square(a);
+    printf("result2: a=%i\n", a); 
+    return 0;
+}
+*/
+
+int main() {
+    return 0;
+}
+
