e704d3b1212d5b0224435f2d006725878f2e15ee
youxg12 <zero_121@sookmyung.ac.kr>
2024-10-30 14:15:27 +0900

    3

---
+++ week9/3/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+
+void f(void); 
+
+int i; 
+int main(void)
+{
+    for (i = 0; i < 5; i++)
+    {
+
+        f(); 
+	}	
+    return 0; 
+}
+
+void f(void)
+{
+    for (i = 0; i < 10; i++) 
+        printf("#"); 
+    
+}
+
