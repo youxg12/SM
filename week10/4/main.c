e673e3340c3768c06f1ce8298839197fc1000d14
youxg12 <zero_121@sookmyung.ac.kr>
2024-11-06 13:34:21 +0900

    4

---
+++ week10/4/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define SIZE 4
+
+void square_array(int a[], int size);
+void print_array(int a[], int size);
+
+int main(void) {
+    int list[SIZE] = {1, 2, 3, 4};
+
+    print_array(list, SIZE);
+    square_array(list, SIZE);
+    print_array(list, SIZE);
+
+    return 0;
+}
+
+void square_array(int a[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        a[i] = a[i] * a[i];
+    }
+}
+
+void print_array(int a[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%3d", a[i]);
+    }
+    printf("\n");
+}
+
