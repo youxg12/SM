3ef9810b397f5724c08388a406717e78d12b83db
youxg12 <zero_121@sookmyung.ac.kr>
2024-11-06 13:57:16 +0900

    2-1

--- week10/2/2-1/main.c
+++ week10/2/2-1/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define SIZE 5
 
 int main(void) {
-    int i, average;
-    int sum;
+    int i;
     int grade[SIZE];
 
-    sum = 0;
-    for (i = 0; i < SIZE; i++) {
-        printf("학생 성적을 입력하세요 : ");
+    printf("5명의 점수를 입력하세요.\n");
+    for(i = 0; i < SIZE; i++) {
         scanf("%d", &grade[i]);
-        sum += grade[i];
     }
 
-    average = sum / SIZE;
-    printf("성적 평균 : %d\n", average);
+    for(i = 0; i < SIZE; i++) {
+        printf("grade[%d] = %d\n", i, grade[i]);
+    }
 
     return 0;
 }
+
