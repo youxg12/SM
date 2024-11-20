26778b7d01fc2367e523155ab3f19a74dd27953c
youxg12 <zero_121@sookmyung.ac.kr>
2024-11-20 12:20:21 +0900

    1

---
+++ week11/1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char letter = 'A'; 
+    const char constantLetter = 'B'; 
+
+    printf("문자 변수: %c\n", letter);
+    printf("문자 상수: %c\n", constantLetter);
+
+    return 0;
+}
+
